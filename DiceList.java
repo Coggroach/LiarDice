40c73d019b116ca0171edf4caa10282d90cf2ab0
Coggroach <Coggroach@users.noreply.github.com>
2015-06-08 16:53:14 +0100

    Create DiceList.java
    Basic DiceList added.

---
+++ DiceList.java
@@ -0,0 +1,77 @@
+package com.coggroach.dice;
+
+public class DiceList
+{
+	public static final int ONE_DOUBLE = 0;
+	public static final int ONE_TRIPLE = 1;
+	public static final int TWO_DOUBLES = 2;
+	public static final int ONE_QUADRIPLE = 3;
+	public static final int ONE_FULLHOUSE = 4;
+	public static final int ONE_STRAIGHT = 5;
+	public static final int ONE_QUINTUPLE = 6;
+	public static final int ONE_NONE = -1;
+	
+	private List<Dice> dice;
+	
+	public DiceList()
+	{
+		this.dice = new ArrayList<Dice>();
+	}
+	
+	public DiceList(int i)
+	{
+		this.dice = new ArrayList<Dice>(i);
+	}
+	
+	public void save(DiceList list)
+	{
+		this.dice.addAll(list.dice);
+	}
+	
+	public void removeAll()
+	{
+		this.dice.removeAll();
+	}
+	
+	public void rollAll()
+	{
+		for(Dice d : dice)
+		{
+			d.roll();
+		}
+	}
+	
+	public void roll(int i)
+	{
+		this.dice.get(i).roll();
+	}
+	
+	public int compare()
+	{
+		
+		return ONE_NONE;
+	}
+	
+	private int getMostFrequent()
+	{
+		int[] list = new int[] {0, 0, 0, 0, 0, 0};
+		
+		for(Dice d : dice)
+		{
+			list[d.getValue()]++;
+		}
+		
+		int maxVal = list[0];
+		int maxFreq = 0;
+		
+		for(int i = 1; i < list.length; i++)
+		{
+			if(maxVal < list[i])
+			{
+				maxVal = list[i];
+				maxFreq = i;
+			}
+		}
+		return maxFreq;
+	}
+}
